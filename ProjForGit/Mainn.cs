de6ac4a6bbb77fe1699d95e9adc7e02888651431
Terest1an <nktv21info@gmail.com>
2023-08-23 21:33:49 +0300

    new Main

---
+++ ProjForGit/Mainn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjForGit
+{
+    internal class Mainn
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
