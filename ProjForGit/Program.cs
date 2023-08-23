9dcd7c4e146a984dc2ffbe5f4a362daee8a684e1
Terest1an <nktv21info@gmail.com>
2023-08-23 14:06:17 +0300

    Решил под задачу №1

--- ProjForGit/Program.cs
+++ ProjForGit/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace ProjForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // изменение для подзадачи1
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
