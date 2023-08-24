ac1849a4d922edc868c9dfa93d7d6d9609572768
terest1an <128174559+Terest1an@users.noreply.github.com>
2023-08-24 17:28:30 +0300

    Add files via upload

---
+++ ProjForGit/Mainn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjForGit
+{
+    internal class Mainn
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //sumtask_1 changes
+            //sumtask_2 changes
+            // изменения не в той ветке
+        }
+    }
+}
---
+++ ProjForGit/book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjForGit
+{
+    internal class book
+    {
+        //123123dasd
+    }
+}
